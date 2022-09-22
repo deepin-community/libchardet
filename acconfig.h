ccaf96169368c4996031bc19ea5cb81ca2e58345
Deepin Developer <deepin-dev@deepin.org>
2022-09-22 17:52:12 +0800

    feat: Init commit
    init libchardet
    
    Log:

---
+++ acconfig.h
@@ -0,0 +1,12 @@
+/* $Id: acconfig.h 19 2011-05-01 15:19:14Z oops $ */
+#ifndef CHARDEC_CONFIG_H
+#define CHARDEC_CONFIG_H
+
+
+/* config.h:  a general config file */
+
+@TOP@
+
+@BOTTOM@
+
+#endif /*  CHARDEC_CONFIG_H */
